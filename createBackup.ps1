5ad63dc0beac0fed77294c197a846a08f03c69d2
roll3d4s <roll.3d4s@gmail.com>
2026-05-21 00:25:05 -0400

    changed the name of the folder created in the backup location

--- createBackup.ps1
+++ createBackup.ps1
@@ -1,5 +1,5 @@
-$date_time = Get-Date -Format "dddd-yyyy-MM-dd-HH-mm"
+$date_time = Get-Date -Format "yyyy-MMM-dd_HHmmss"
 $backup_location = 'C:\Users\Cass\Documents\MinecraftServer\Backups\'+$date_time
 
 New-Item -Path $backup_location -ItemType Directory
-Copy-Item -Path 'C:\Users\Cass\Documents\MinecraftServer\Server\world' -Destination 'C:\Users\Cass\Documents\MinecraftServer\Backups\$date_time' -Recurse
+Copy-Item -Path 'C:\Users\Cass\Documents\MinecraftServer\Server\world' -Destination $backup_location -Recurse
