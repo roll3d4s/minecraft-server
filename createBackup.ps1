4158cd1cea5259331bf9939cf27e69ceaa8a9f3f
roll3d4s <roll.3d4s@gmail.com>
2026-05-21 00:29:23 -0400

    world folder is moved, and then renamed instead of moved into pre-named folder

--- createBackup.ps1
+++ createBackup.ps1
@@ -1,5 +1,9 @@
 $date_time = Get-Date -Format "yyyy-MMM-dd_HHmmss"
-$backup_location = 'C:\Users\Cass\Documents\MinecraftServer\Backups\'+$date_time
+# $backup_location = 'C:\Users\Cass\Documents\MinecraftServer\Backups\'+$date_time
 
-New-Item -Path $backup_location -ItemType Directory
-Copy-Item -Path 'C:\Users\Cass\Documents\MinecraftServer\Server\world' -Destination $backup_location -Recurse
+# New-Item -Path $backup_location -ItemType Directory
+
+# Copy-Item -Path 'C:\Users\Cass\Documents\MinecraftServer\Server\world' -Destination $backup_location -Recurse
+
+Copy-Item -Path 'C:\Users\Cass\Documents\MinecraftServer\Server\world' -Destination 'C:\Users\Cass\Documents\MinecraftServer\Backups\' -Recurse
+Rename-Item -Path 'C:\Users\Cass\Documents\MinecraftServer\Backups\world' -NewName $date_time
