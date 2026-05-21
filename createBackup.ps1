c8c4d7defb90482273ee10c5b9069be5ea6ce8f9
roll3d4s <roll.3d4s@gmail.com>
2026-05-21 00:17:52 -0400

    script creates a new folder in backup location

--- createBackup.ps1
+++ createBackup.ps1
@@ -1,4 +1,5 @@
-$date_time = Get-Date -Format "dddd-yyyy-MM-dd-HH:mm"
+$date_time = Get-Date -Format "dddd-yyyy-MM-dd-HH-mm"
+$backup_location = 'C:\Users\Cass\Documents\MinecraftServer\Backups\'+$date_time
 
-New-Item -Path "C:\Users\Cass\Documents\Minecraft Servers\Backups\$date_time" -ItemType Directory
-Copy-Item -Path "C:\Users\Cass\Documents\Minecraft Server\Server\world" -Destination "C:\Users\Cass\Documents\Minecraft Server\Backups\$date_time" -Recurse
+New-Item -Path $backup_location -ItemType Directory
+Copy-Item -Path 'C:\Users\Cass\Documents\MinecraftServer\Server\world' -Destination 'C:\Users\Cass\Documents\MinecraftServer\Backups\$date_time' -Recurse
