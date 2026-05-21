d21e5e19df9c6409ceebcd1e28cb86d153ce2f9e
roll3d4s <roll.3d4s@gmail.com>
2026-05-21 00:01:45 -0400

    made it so script creates a new folder to copy world backup into

--- createBackup.ps1
+++ createBackup.ps1
@@ -1,1 +1,4 @@
-Copy-Item -Path "C:\Users\Cass\Documents\Minecraft Server\Server\world" -Destination "C:\Users\Cass\Documents\Minecraft Server\Backups" -Recurse
+$date_time = Get-Date -Format "dddd-yyyy-MM-dd-HH:mm"
+
+New-Item -Path "C:\Users\Cass\Documents\Minecraft Servers\Backups\$date_time" -ItemType Directory
+Copy-Item -Path "C:\Users\Cass\Documents\Minecraft Server\Server\world" -Destination "C:\Users\Cass\Documents\Minecraft Server\Backups\$date_time" -Recurse
